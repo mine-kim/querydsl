5f18bd9fa4bbd2fac520f20baf846bb45280226c
kmh <minhye0619@gmail.com>
2024-07-20 15:51:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# querydsl
+
+## 개요
+실전! Querydsl<br/>
+site: [실전! Querydsl](https://www.inflearn.com/course/lecture?courseSlug=querydsl-%EC%8B%A4%EC%A0%84&unitId=30122&tab=curriculum)
+## 개발 환경
+- Java 17
+- Springboot 3.3.0
+- h2 2.2.224
+
+## 커리큘럼
+1. 프로젝트 환경설정
+2. 예제 도메인 모델
+3. 기본 문법
+4. 중급 문법
+5. 실무 활용 - 순수 JPA와 Querydsl
+6. 실무 활용 - 스프링 데이터 JPA와 Querydsl
+7. 스프링 데이터 JPA가 제공하는 Querydsl 기능
---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,33 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+@SpringBootTest
+@Transactional
+@Commit
+class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em); QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+    }
+
+}
